69bc75ef35cc104142b1ccce1b96437fdb457ca3
Kale Leach <kaleleach@Jarmans-MacBook-Pro.local>
2025-01-07 20:32:28 -0800

    Day 2.2 User Input

---
+++ src/VarSwap.java
@@ -0,0 +1,17 @@
+package src;
+public class VarSwap {
+
+    public static void main(String[] args) {
+        String x = "Water";
+        String y = "Kool-aid";
+        String temp;
+        
+        temp = x;
+        x = y;
+        y = temp;
+
+        System.out.println("x: " +x);
+        System.out.println("y: " +y);
+
+    }
+}
