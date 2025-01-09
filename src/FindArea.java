92889d2bfe5718fa3b5fee2a32fa555a323d9680
Kale Leach <kaleleach@Jarmans-MacBook-Pro.local>
2025-01-08 20:33:24 -0800

    Day 3.2 Random random

--- src/FindArea.java
+++ src/FindArea.java
@@ -1,7 +1,7 @@
 package src;
 import java.util.Scanner;
 
-public class FindWidth {
+public class FindArea {
 
     public static void main(String[] args) {
         double width = 0;
---
+++ src/RandomNumbers.java
@@ -0,0 +1,23 @@
+package src;
+import java.util.Random;
+
+public class RandomNumbers {
+    public static void main(String[] args) {
+        
+        Random random = new Random();
+
+        int nubmer;
+        // Will print nubmer between -2 billion and 2 billion
+        // nubmer = random.nextInt();
+        // The bound (2nd number bust be one higher than the last nubmer needed in the random generator. Below === 1 - 100. If I put 1, 100 it would be 1-99 )
+        nubmer = random.nextInt( 1, 101);
+
+        boolean isHeads;
+
+        isHeads = random.nextBoolean();
+
+
+        System.out.println(nubmer);
+        System.out.println(isHeads);
+    }
+}
