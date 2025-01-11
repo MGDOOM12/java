06075c1cf5c1853a9f3059bcc3e31184e53fcf81
Kale Leach <kaleleach@Jarmans-MacBook-Pro.local>
2025-01-11 15:48:37 -0800

    Day 5.3 Substring

---
+++ Main/Substrings.java
@@ -0,0 +1,16 @@
+package Main;
+
+public class Substrings {
+    public static void main(String[] args) {
+        // .substring() is a method used to extract a portion of a string
+        char firstCharDomain = '@';
+        String email = "kale.leach@gmail.com";
+        String email2 = "C.C@outlook.com";
+        String username = email2.substring(0,email2.indexOf(firstCharDomain));
+        String domain = email.substring(email.indexOf(firstCharDomain), email.length());
+        
+
+        System.out.println(username);
+        System.out.println(domain);
+    }
+}
