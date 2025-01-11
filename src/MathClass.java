b1469bbabd7a981ce3c8296abd81498dc61cecbd
Kale Leach <kaleleach@Jarmans-MacBook-Pro.local>
2025-01-10 17:45:03 -0800

    Day 4.1 Math

---
+++ src/MathClass.java
@@ -0,0 +1,28 @@
+package src;
+
+public class MathClass {
+
+    public static void main(String[] args) {
+        // Math methods to get both PI and E
+        System.out.println(Math.PI);
+        System.out.println(Math.E);
+    
+        // Math method to raise numbers to a given power.
+        System.out.println(Math.pow(2,3));
+        // Absoulute will return any number postive. It is determining the number distance from 0.
+        System.out.println(Math.abs(-5));
+        // Sqaure root
+        System.out.println(Math.sqrt(25));
+        // Math.round will round to the nearest whole integer
+        System.out.println(Math.round(3.14));
+        // Math.ceil will always round up
+        System.out.println(Math.ceil(3.14));
+        // Math.floor will always round down
+        System.out.println(Math.floor(3.99));
+        // Math.max will find the greater number between two numbers 
+        System.out.println(Math.max(1,3));
+        // Math.min will find the smallest number between two numbers
+        System.out.println(Math.min(1,3));
+
+    }
+}
